d45f768e9f4eec26b3816bbb6be291b09ab2294c
caofanCPU <caofan@huitongjy.com>
2020-04-17 02:30:15 +0800

    support concurrent request, but somewhere has bug in httpie util.

--- auto/batchHandler.py
+++ auto/batchHandler.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import datetime
 import json
 import ssl
-import sys
 import subprocess
+import sys
 import time
-import pandas as pd
+from concurrent.futures.thread import ThreadPoolExecutor
 
+import pandas as pd
 import requests
 
 # 屏蔽HTTPS证书校验, 忽略安全警告
@@ -18,22 +20,28 @@ id_key = 'NONE'
 hot_reload = True
 cmd = "http"
 no_ca = "--verify=no"
-httpie_allow_view = {
-    "-v": "显示请求详细信息",
-    "-h": "显示请求头",
-    "-b": "显示请求Body",
-    "-d": "响应结果保存至TXT",
-    "": "默认"
-}
+httpie_allow_view = {"-v": "显示请求详细信息", "-h": "显示请求头", "-b": "显示请求Body", "-d": "响应结果保存至TXT", "": "默认"}
 httpie_view = None
+# 并发数
+max_concurrent = 64
+concurrent = 1
 try:
     if len(sys.argv) > 1:
         if httpie_allow_view.get(sys.argv[1]) is not None:
             httpie_view = sys.argv[1]
         else:
             print("输入参数有误, 仅支持如下参数: -v显示请求详细信息|-h显示请求头|-b显示请求Body|-d响应结果保存至TXT")
+    if len(sys.argv) > 2:
+        try:
+            input_concurrent = int(sys.argv[2])
+            if input_concurrent > 1:
+                concurrent = min(input_concurrent, max_concurrent)
+        except Exception as e:
+            print("并发数设置范围[1, {}], 默认1".format(max_concurrent))
+            print(e)
 except Exception as e:
     print(e)
+executor = ThreadPoolExecutor(max_workers=concurrent)
 
 
 def httpie_cmd(id):
@@ -55,11 +63,13 @@ def httpie_cmd(id):
         httpie_params.append(httpie_view)
     httpie_params.extend([method, url, headers, body])
     httpie = joiner.join(httpie_params)
-    print(httpie)
-    print("当前ID: ", id)
+    # print(httpie)
+    executeStartTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
     # 延时执行
-    time.sleep(0.05)
+    # time.sleep(0.05)
     subprocess.call(httpie, shell=True)
+    executeEndTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+    return "当前[ID={}, [executeStartTime={}], [executeEndTime={}]".format(id, executeStartTime, executeEndTime)
 
 
 def load_request_headers(headers, hot_reload):
@@ -113,15 +123,16 @@ def auto_login():
         f.write(cookie)
     # JSON标准格式
     response_body = json.dumps(response.json(), ensure_ascii=False, indent=4)
-    print(response_body)
+    print("\n执行登录响应BODY结果: \n" + response_body)
     return cookie
 
 
 def main():
     # 首先登陆一次
     auto_login()
-    for id in load_data():
-        httpie_cmd(id)
+    ids = load_data()
+    for result in executor.map(httpie_cmd, ids):
+        print(result)
 
 
 if __name__ == '__main__':
