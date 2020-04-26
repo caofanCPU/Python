2e2b3a6a514bd74959f9d217278b5b33088af301
caofan <caofan@huitongjy.com>
2020-04-26 17:10:52 +0800

    wtf...

--- auto/batchHandler.py
+++ auto/batchHandler.py
@@ -3,7 +3,7 @@
 import datetime
 import json
 import ssl
-import subprocess
+import unicodedata
 import sys
 import time
 from concurrent.futures.thread import ThreadPoolExecutor
@@ -15,9 +15,6 @@ import requests
 requests.packages.urllib3.disable_warnings()
 context = ssl._create_unverified_context()
 joiner = ' '
-id_key = 'NONE'
-# 登陆一次后是否服复用cookie
-hot_reload = True
 cmd = "http"
 no_ca = "--verify=no"
 httpie_allow_view = {"-v": "显示请求详细信息", "-h": "显示请求头", "-b": "显示请求Body", "-d": "响应结果保存至TXT", "": "默认"}
@@ -25,8 +22,6 @@ httpie_view = None
 # 并发数
 max_concurrent = 64
 concurrent = 1
-# 是否开启并发测试
-enable_concurrent_test = True
 try:
     if len(sys.argv) > 1:
         if httpie_allow_view.get(sys.argv[1]) is not None:
@@ -56,64 +51,60 @@ def httpie_cmd(id):
         request_json = json.load(request_data)
     url = request_json['url']
     method = request_json['method']
-    request_headers = request_json['headers']
-    cookie = load_cookie(hot_reload)
-    request_headers.append('Cookie:' + cookie)
-    headers = joiner.join(request_headers)
-    body = joiner.join(request_json['body'])
-    body = body.replace(id_key, str(id))
-    httpie_params = [cmd, no_ca]
-    if httpie_view is not None:
-        httpie_params.append(httpie_view)
-    httpie_params.extend([method, url, headers, body])
-    httpie = joiner.join(httpie_params)
+    request_headers = handle_json_str_value(request_json['headers'])
+    request_headers['Cookie'] = init_cookie
+    request_body = replace_id(request_json['body'], id)
+    response_body = {
+        "status": -1,
+        "msg": "接口执行失败",
+        "data": "请检查接口是否返回JSON格式的相应数据, 以及抛出未经处理的特殊异常"
+    }
     executeStartTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-    # 延时执行
-    # time.sleep(0.05)
-    # 使用httpie执行shell, 线程套线程, 效率低下
-    # subprocess.call(httpie, shell=True)
-    response_body = execute(request_json, cookie, id)
-    executeEndTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
-    return "执行命令\n{}\n当前[ID=[{}], executeStartTime=[{}], executeEndTime=[{}]\n响应结果:\n{}".format(httpie, id, executeStartTime, executeEndTime, response_body)
-
-
-def execute(request_json, cookie, id):
-    url = request_json['url']
-    method = request_json['method']
-    request_headers = {}
-    for item in request_json['headers']:
-        if item.find('Cookie') != -1:
-            continue
-        split = item.replace('=', '').split(':')
-        request_headers[split[0]] = split[1]
-    request_headers['Cookie'] = cookie
-    request_body = {}
-    for item in request_json['body']:
-        split = item.replace(':', '').replace('NONE', str(id)).split('=')
-        request_body[split[0]] = split[1]
     try:
         response = requests.request(method, url, headers=request_headers, json=request_body, timeout=3, verify=False)
         # JSON标准格式
         response_body = json.dumps(response.json(), ensure_ascii=False, indent=4)
     except Exception as e:
-        response_body = "服务方接口执行失败\n"
-        print(response)
         print(e)
-    return response_body
+    executeEndTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+    httpie_cmd_str = httpie(url, method, request_headers, request_body)
+    return "执行命令httpie:\n{}\n当前ID=[{}], executeStartTime=[{}], executeEndTime=[{}]\n响应结果:\n{}".format(httpie_cmd_str, id, executeStartTime, executeEndTime, response_body)
 
 
-def load_cookie(reuse_cookie):
-    """
-    加载cookie
-    :param reuse_cookie: 是否热加载, 复用已有Cookie
-    :return:
-    """
-    if reuse_cookie:
-        with open("./cookie.txt", "r") as f:
-            cookie = ''.join(f.readlines())
-    else:
-        cookie = auto_login()
-    return cookie
+def httpie(url, method, request_headers, request_body):
+    param_list = [cmd, no_ca]
+    if httpie_view is not None:
+        param_list.append(httpie_view)
+    param_list.extend([method, url])
+    for (k, v) in request_headers.items():
+        if k == "Cookie":
+            param_list.append("'" + k + ":" + v + "'")
+        else:
+            param_list.append(k + ":" + v)
+    for (k, v) in request_body.items():
+        if is_number(v):
+            param_list.append(k + ":=" + v)
+        else:
+            param_list.append(k + "=" + v)
+    return joiner.join(param_list)
+
+
+def is_number(s):
+    try:
+        # 如果能运行float(s)语句，返回True（字符串s是浮点数）
+        float(s)
+        return True
+    except ValueError:
+        # ValueError为Python的一种标准异常，表示"传入无效的参数"
+        # 如果引发了ValueError这种异常，不做任何事情（pass：不做任何事情，一般用做占位语句）
+        pass
+    try:
+        # 把一个表示数字的字符串转换为浮点数返回的函数
+        unicodedata.numeric(s)
+        return True
+    except (TypeError, ValueError):
+        pass
+    return False
 
 
 def load_data():
@@ -128,57 +119,58 @@ def load_data():
 
 def auto_login():
     """
-    自动登录, 获取登录Cookie, 写入文件, 在控制台打印
+    自动登录, 获取登录Cookie
     """
     with open("./ssoLogin.json", 'r') as sso_login_request_data:
         request_json = json.load(sso_login_request_data)
     url = request_json['url']
     method = request_json['method']
-    request_headers = {}
-    for item in request_json['headers']:
-        split = item.replace('=', '').split(':')
-        request_headers[split[0]] = split[1]
-    request_body = {}
-    for item in request_json['body']:
-        split = item.replace(':', '').split('=')
-        request_body[split[0]] = split[1]
-
-    request_headers = {"Content-Type": "application/json", "HT-app": "6"}
+    request_headers = handle_json_str_value(request_json['headers'])
+    request_body = handle_json_str_value(request_json['body'])
+    # request_headers = {"Content-Type": "application/json", "HT-app": "6"}
     response = requests.request(method, url, headers=request_headers, json=request_body, timeout=3, verify=False)
     response_headers = response.headers
     cookie = response_headers.get("set-Cookie")
-    with open("./cookie.txt", "w") as f:
-        f.write(cookie)
     # JSON标准格式
     response_body = json.dumps(response.json(), ensure_ascii=False, indent=4)
-    print("\n执行登录响应BODY结果: \n" + response_body)
+    print("登录响应Cookie结果: \n{}\n登录响应BODY结果: {}".format(cookie, response_body))
     return cookie
 
 
-def normal():
-    ids = load_data()
-    for result in executor.map(httpie_cmd, ids):
-        print(result)
+def handle_json_str_value(json):
+    """
+    将json的值都变为字符串处理
+    :param json:
+    :return:
+    """
+    for (k, v) in json.items():
+        json[k] = str(v)
+    return json
 
 
-def concurrent_infinite_loop_test():
-    # datas = list(range(1, 20))
-    datas = [10009984, 10009984, 10009984, 10009984, 10009984, 10009984, 10009984, 10009984, 10009984, 10009984, \
-             10009984, 10009984, 10009984, 10009984, 10009984, 10009984, 10009984, 10009984, 10009984, 10009984, \
-            ]
-
-    while True:
-        for result in executor.map(httpie_cmd, datas):
-            print(result)
+def replace_id(json, id):
+    """
+    将json的值都变为字符串处理
+    :param json:
+    :return:
+    """
+    for (k, v) in json.items():
+        if v == "NONE":
+            json[k] = str(id)
+        else:
+            json[k] = str(v)
+    return json
 
 
 def main():
+    # 全局变量cookie, 初始化为空
+    global init_cookie
     # 首先登陆一次
-    auto_login()
-    if enable_concurrent_test:
-        concurrent_infinite_loop_test()
-    else:
-        normal()
+    init_cookie = auto_login()
+    # 读取ID数据列表
+    ids = load_data()
+    for result in executor.map(httpie_cmd, ids):
+        print(result)
 
 
 if __name__ == '__main__':
